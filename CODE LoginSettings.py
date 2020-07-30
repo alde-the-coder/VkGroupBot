b82449a828d0273d7970c0bbdd9fd291e9b8c5fc
Alexey <aldethecoder@gmail.com>
2020-07-30 20:41:12 +0300

    Source code V1.1

--- CODE LoginSettings.py
+++ CODE LoginSettings.py
@@ -25,12 +25,8 @@ def cicle():
     f.write(msgb + '\n')
     f.write(msgu + '\n')
     f.close()
-    mchoice = input(
-        "Press 1 if you want to save and exit, 2 if you want to make more messages\n")
+    mchoice = input("Press 1 if you want to save and exit, 2 if you want to make more messages\n")
     if mchoice == "1":
-        lines = [" "]*1000
-        with open("settings.txt", "a") as file:
-            file.writelines("%s\n" % line for line in lines)
             global q
             q = q-1
     if choice == "2":
@@ -40,4 +36,4 @@ while q>1 :
     cicle()
 
 #made by alde-the-coder
-#version 1.0
+#version 1.1
