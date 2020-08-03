e0354e6515c8e1323af3237ce84ee69757f1e1b4
Alexey <aldethecoder@gmail.com>
2020-08-04 00:06:52 +0300

    Source code v1.3a

--- vgb.py
+++ vgb.py
@@ -3,6 +3,7 @@ import random
 import time
 a=3
 b=3
+botison=2
 while True:
     check = input("Do you wanna to turn on the bot OR to rewrite/write the settings.txt? 1/2 \n")
     if check=="1":
@@ -10,6 +11,7 @@ while True:
     if check=="2":
         f = open("settings.txt", "w")
         f.close()
+        botison=botison-2
         while a>2:
             token = input("Enter your token \n")
             choice = input("Enter 1 if you want to continue, 2 if you want to change your token\n")
@@ -29,42 +31,41 @@ while True:
             f.write(msgb + '\n')
             f.write(msgu + '\n')
             f.close()
-            mchoice = input("Press 1 if you want to save and turn on the bot, 2 if you want to make more messages\n")
+            mchoice = input("Press 1 if you want to save, 2 if you want to make more messages\n")
             if mchoice == "1":
                 b=b-2
             if mchoice == "2":
                 continue
 
-x = 1
-y = 2
-
-f = open("settings.txt", 'r')
-lin = f.readlines()
-token = (lin[0]).rstrip("\n")
-
-vk = vk_api.VkApi(token=token)
-vk._auth_token()
-
-print("Bot is working!")
-
-while True:
-    try:
-        messages = vk.method("messages.getConversations", {"offset": 0, "count": 1, "filter": "unanswered"})
-        if messages["count"] >= 1:
-            id = messages["items"][0]["last_message"]["from_id"]
-            body = messages["items"][0]["last_message"]["text"]
-            if body.lower() != lin[x].rstrip("\n"):
-                x = x + 2
-                y = y + 2
-                continue
-            elif body.lower() == lin[x].rstrip("\n"):
-                vk.method("messages.send", {"peer_id": id, "message": lin[y].rstrip("\n"), "random_id": random.randint(1, 2147483647)})
-                x = 1
-                y = 2
-                continue
-
-    except Exception as E:
-        time.sleep(1)
+if botison>1:
+    x = 1
+    y = 2
+    f = open("settings.txt", 'r')
+    lin = f.readlines()
+    token = (lin[0]).rstrip("\n")
+    vk = vk_api.VkApi(token=token)  
+    vk._auth_token()
+    print("Bot is working!")
+    while True:
+        try:
+            messages = vk.method("messages.getConversations", {"offset": 0, "count": 1, "filter": "unanswered"})
+            if messages["count"] >= 1:
+                id = messages["items"][0]["last_message"]["from_id"]
+                word = body = messages["items"][0]["last_message"]["text"]
+                filename = 'settings.txt'
+                with open(filename) as file:
+                    if word in file.read():
+                        with open('settings.txt', 'rt') as file:
+                            for index, line in enumerate(file):
+                                if word in line:
+                                        f = open("settings.txt", 'r')
+                                        lin = f.readlines()
+                                        answer = (lin[index+1]).rstrip("\n")
+                        vk.method("messages.send", {"peer_id": id, "message": answer, "random_id": random.randint(1, 2147483647)})
+        except Exception as E:
+            time.sleep(1)
+else:
+    input("Please reopen the application")
 
 #made by alde-the-coder
-#version 1.2
+#version 1.3a
