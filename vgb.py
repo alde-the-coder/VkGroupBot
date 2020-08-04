d117890a2520850a4842b7cf61a0407336cae898
Alexey <aldethecoder@gmail.com>
2020-08-04 13:56:18 +0300

    Source code V1.3b

--- vgb.py
+++ vgb.py
@@ -17,7 +17,8 @@ while True:
             choice = input("Enter 1 if you want to continue, 2 if you want to change your token\n")
             if choice == "1":
                 f = open("settings.txt", "a")
-                f.write(token + '\n')
+                f.write(token + "\n")
+                f.write("\n")
                 f.close()
                 a=a-2
             if choice == "2":
@@ -28,8 +29,9 @@ while True:
             msgb = input("Enter user message to bot \n")
             msgu = input("Enter bot message to user \n")
             f = open("settings.txt", "a")
-            f.write(msgb + '\n')
-            f.write(msgu + '\n')
+            f.write(msgb + "\n")
+            f.write(msgu + "\n")
+            f.write("\n")
             f.close()
             mchoice = input("Press 1 if you want to save, 2 if you want to make more messages\n")
             if mchoice == "1":
@@ -40,7 +42,7 @@ while True:
 if botison>1:
     x = 1
     y = 2
-    f = open("settings.txt", 'r')
+    f = open("settings.txt", "r")
     lin = f.readlines()
     token = (lin[0]).rstrip("\n")
     vk = vk_api.VkApi(token=token)  
@@ -52,20 +54,23 @@ if botison>1:
             if messages["count"] >= 1:
                 id = messages["items"][0]["last_message"]["from_id"]
                 word = body = messages["items"][0]["last_message"]["text"]
-                filename = 'settings.txt'
+                filename = "settings.txt"
                 with open(filename) as file:
                     if word in file.read():
-                        with open('settings.txt', 'rt') as file:
+                        with open("settings.txt", "rt") as file:
                             for index, line in enumerate(file):
                                 if word in line:
-                                        f = open("settings.txt", 'r')
+                                        f = open("settings.txt", "r")
                                         lin = f.readlines()
                                         answer = (lin[index+1]).rstrip("\n")
-                        vk.method("messages.send", {"peer_id": id, "message": answer, "random_id": random.randint(1, 2147483647)})
+                                        if answer != "":
+                                            vk.method("messages.send", {"peer_id": id, "message": answer, "random_id": random.randint(1, 2147483647)})
+                                        else:
+                                            continue
         except Exception as E:
             time.sleep(1)
 else:
     input("Please reopen the application")
 
 #made by alde-the-coder
-#version 1.3a
+#version 1.3b
