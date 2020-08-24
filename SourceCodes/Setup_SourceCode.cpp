24e874885daaa10241529ac0ea5bfe68fc896ebb
Alexey <68921512+alde-the-coder@users.noreply.github.com>
2020-08-25 02:04:23 +0300

    Source codes

---
+++ SourceCodes/Setup_SourceCode.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Downloading vk_api..." << endl;
+	system("pip install vk_api");
+	cout << "Done, you can close the window now." << endl;
+	system("pause");
+	return 0;
+}
